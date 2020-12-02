3396afdc9850a31c88e128f2f06521136352939d
SimonRuckli <simon_ruckli@sluz.ch>
2020-12-02 09:29:04 +0100

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,9 +1,21 @@
-import { serve } from 'https://deno.land/std/http/server.ts'
-const s = serve({ port: 8000 })
-console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`)
-for await (const req of s) {
-    req.respond({ body: 'Hello from your first Deno a' })
-}
+import { Application, Router, send } from "https://deno.land/x/oak@v6.3.1/mod.ts"
+
+const app = new Application();
+const router = new Router();
+
+router
+    .get("/", (context) => {
+        return send(context, "web/index.html");
+    })
+    .get("/files/:fileName", (context) => {
+        const fileName = context.params.fileName;
+        return send(context, "web/" + fileName)
+    })
+
+app.use(router.routes());
+app.listen({ port: 8000 });
+
+
 
 /*import { Application, Router, send, } from "https://deno.land/x/oak@v6.3.1/mod.ts";
 const app = new Application();
