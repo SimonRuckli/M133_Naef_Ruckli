523ae96285c4eb5f63cf41b3f1d4d141b31b021a
Shadow <Cedric_Naef@sluz.ch>
2020-12-02 09:28:06 +0100

    Cards can be added

--- web/weblogic.js
+++ web/weblogic.js
@@ -1,12 +1,16 @@
-import { Application, Router, send, } from "https://deno.land/x/oak@v6.3.1/mod.ts";
-const app = new Application();
-const router = new Router();
-router
-  .get("/directText", (context) => {
-    context.response.body = "some text";
-  })
-  .get("/file", (context) => {
-    return send(context, "index.html");
-  });
-app.use(router.routes());
-app.listen( { port: 8080 } );
+console.log("1");
+
+var tag = document.createElement("div");
+console.log("2");
+
+var text = document.createTextNode("implementieren");
+console.log("3");
+
+tag.appendChild(text);
+console.log("4");
+
+var element = document.getElementById("taskToDo");
+console.log("5");
+
+element.appendChild(tag);
+console.log("6");
