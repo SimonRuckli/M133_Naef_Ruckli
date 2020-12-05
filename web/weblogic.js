cb34b3cc929478d2321438f3c12bb8b67c949cfe
SimonRuckli <55438072+SimonRuckli@users.noreply.github.com>
2020-12-05 12:03:09 +0100

    new initial commit - create UI by js

--- web/weblogic.js
+++ web/weblogic.js
@@ -1,16 +1,52 @@
-console.log("1");
+let column = 0;
+let dragedtaskid = "";
+loadPage();
 
-var tag = document.createElement("div");
-console.log("2");
+async function loadPage() {
+    document.getElementById("tasks").innerHTML = "";
 
-var text = document.createTextNode("implementieren");
-console.log("3");
+    let columns;
+    let tasks;
 
-tag.appendChild(text);
-console.log("4");
+    try {
+        let columns_response = await fetch("/Columns");
+        columns = await columns_response.json();
+        let tasks_response = await fetch("/Tasks");
+        tasks = await tasks_response.json();  
+    } catch(err) {
+        console.log(err);
+        return;
+    }
 
-var element = document.getElementById("taskToDo");
-console.log("5");
+    for (i = 0; i < columns.length; i++) {
+        let div = document.createElement("div");
+        div.className = "column";
+        div.id = "Column_" + columns[i].Title.replace(" ", "");
+        div.style.height = "100vh";
+        div.style.width = (100 / columns.length).toFixed(1) + "%";
+        if (i % 2 == 1) {
+            div.style.backgroundColor = "ghostwhite";
+        }
 
-element.appendChild(tag);
-console.log("6");
+        let divtitle = document.createElement("div");
+        divtitle.className = "column_title";
+        divtitle.style.backgroundColor = columns[i].Color;
+        let title = document.createElement("label");
+        title.innerText = columns[i].Title;
+        divtitle.appendChild(title);
+
+        let divcontent = document.createElement("div");
+        divcontent.className = "column_content";
+        divcontent.id = "Column_Content_" + i;
+        divcontent.style.height = "100vh";
+
+        div.appendChild(divtitle);
+        div.appendChild(divcontent);
+
+        document.getElementById("tasks").appendChild(div);
+    }
+}
+
+
+
+
--- webserver.js
+++ webserver.js
@@ -3,29 +3,35 @@ import { Application, Router, send } from "https://deno.land/x/oak@v6.3.1/mod.ts
 const app = new Application();
 const router = new Router();
 
-router
+let tasks = [];
+const columns = [{
+    Title: "ToDo",
+    Color: "darkorange"
+}, {
+    Title: "In Progress",
+    Color: "lightskyblue"
+}, {
+    Title: "Done",
+    Color: "lightgreen"
+}]
+
+router 
     .get("/", (context) => {
         return send(context, "web/index.html");
     })
+
     .get("/files/:fileName", (context) => {
         const fileName = context.params.fileName;
         return send(context, "web/" + fileName)
     })
 
+    .get("/Tasks", (context) => {
+        context.response.body = tasks;
+    })
+
+    .get("/Columns", (context) => {
+        context.response.body = columns;
+    })
+
 app.use(router.routes());
 app.listen({ port: 8000 });
-
-
-
-/*import { Application, Router, send, } from "https://deno.land/x/oak@v6.3.1/mod.ts";
-const app = new Application();
-const router = new Router();
-router
-  .get("/directText", (context) => {
-    context.response.body = "some text";
-  })
-  .get("/file", (context) => {
-    return send(context, "index.html");
-  });
-app.use(router.routes());
-app.listen( { port: 8080 } );*/
